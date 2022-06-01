099d305abde303e3b4750b31f4cbd8f728c67e14
kumagon <kumagon0619@gmail.com>
2022-06-01 15:05:00 +0900

    エクストラ課題

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
-function clickHandler() {
+function clickHandler(name) {
     const box = document.getElementById("box");
     const p = document.createElement("p");
-    p.innerText = "Added by JS";
+    p.innerText = "Added by " + name;
     box.appendChild(p);
+}
+
+function clickClear() {
+    const box = document.getElementById("box");
+    box.innerHTML = '';
 }
